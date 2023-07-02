5360b733bc00d0d88c4014bcb502c68cf030e222
Lai Thanh Xuan <Xuanlai583@gmail.com>
2023-07-03 02:36:42 +0700

    login

---
+++ main/footer.php
@@ -0,0 +1,18 @@
+	<div class="container">
+		<footer class="border-dark row mt-3 mt-md-5 pt-2">
+			<div class="col-md-3  text-center">
+				(c) HMUSIC - 2022
+			</div>
+			<div class="col-md-3  text-center text-dark">
+				<a target="blank" href="https://www.facebook.com/profile.php?id=100088122387901" title="" class="text-dark">Facebook</a>
+			</div>
+			<div class="col-md-3  text-center text-dark">
+				<a target="blank" href="https://www.instagram.com/" title="" class="text-dark">Instagram</a>
+			</div>
+			<div class="col-md-3 text-center">
+				<a target="blank" href="https://www.youtube.com/channel/UCpc8m3qvHdO01KpcqgHKUog" title="" class="text-dark">YouTube</a>
+			</div>
+		</footer>
+	</div>
+</body>
+</html>
